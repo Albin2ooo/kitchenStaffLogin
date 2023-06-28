47da6d22aaeefc65bc4ea670cfac8575691f4ee4
Albin2ooo <albinjohnson2017@gmail.com>
2023-06-28 11:43:00 +0530

    login page of kitchen staff

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StaffLoginComponent } from './Components/staff-login/staff-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StaffLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
